588eb4165597976fa95ef889c9465b3c8f58b36c
legolas007 <oukuanou@163.com>
2018-03-23 23:52:25 +0800

    添加商品service层

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.e3mall.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.common.utils.IDUtils;
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.pojo.TbItemExample.Criteria;
 import cn.e3mall.service.ItemService;
@@ -24,7 +29,8 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
-
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
 	@Override
 	public TbItem getItemById(long itemId) {
 		// TODO Auto-generated method stub
@@ -60,6 +66,38 @@ public class ItemServiceImpl implements ItemService {
 		return result;
 
 	}
+
+	/**
+	 * 商品添加
+	 */
+	@Override
+	public E3Result addItem(TbItem item, String desc) {
+		// TODO Auto-generated method stub
+		//生成商品id
+		long itemId = IDUtils.genItemId();
+		//补全item的属性
+		item.setId(itemId);
+		//1-正常，2-下架，3-删除
+		item.setStatus((byte) 1);
+		item.setCreated(new Date());
+		item.setUpdated(new Date());
+		//向商品表插入数据
+		itemMapper.insert(item);
+		//创建一个商品描述表对应的pojo对象。
+		TbItemDesc itemDesc = new TbItemDesc();
+		
+		//补全属性
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setCreated(new Date());
+		itemDesc.setUpdated(new Date());
+		//向商品描述表插入数据
+		itemDescMapper.insert(itemDesc);
+		//返回成功
+		return E3Result.ok();
+	}
+	
+	
 	
 
 }
