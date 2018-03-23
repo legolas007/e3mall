3ede39b50111039318184fb074327c393f6ca211
legolas007 <oukuanou@163.com>
2018-03-23 23:14:23 +0800

    id生成工具类

---
+++ e3-common/src/main/java/cn/e3mall/common/utils/IDUtils.java
@@ -0,0 +1,48 @@
+package cn.e3mall.common.utils;
+
+import java.util.Random;
+
+/**
+ * id生成方法
+ * @author usher
+ *
+ */
+public class IDUtils {
+
+	/**
+	 * 生成图片名
+	 * @return
+	 */
+	public static String genImageName() {
+		//取当前时间的长整形值包含毫秒
+		long millis = System.currentTimeMillis();
+		//long millis = System.nanoTime();
+		//加上三位随机数
+		Random random = new Random();
+		int end3 = random.nextInt(999);
+		//如果不足三位前面补0
+		String str = millis + String.format("%03d", end3);
+		
+		return str;
+	}
+	
+	/**
+	 * 商品id
+	 * @return
+	 */
+	public static long genItemId() {
+		
+		long millis = System.currentTimeMillis();
+		
+		Random random = new Random();
+		int end2 = random.nextInt(99);
+		
+		String str = millis + String.format("02d", end2);
+		long id = new Long(str);
+		return id;
+	}
+	public static void main(String[] args) {
+		for(int i=0;i< 100;i++)
+		System.out.println(genItemId());
+	}
+}
