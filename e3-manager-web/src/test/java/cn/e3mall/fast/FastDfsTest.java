a2a17a9f0c14a1071b0a680911055f71646b1463
legolas007 <oukuanou@163.com>
2018-03-23 17:51:42 +0800

    完善图片上传

--- e3-manager-web/src/test/java/cn/e3mall/fast/FastDfsTest.java
+++ e3-manager-web/src/test/java/cn/e3mall/fast/FastDfsTest.java
@@ -7,12 +7,16 @@ import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
 import org.junit.Test;
 
+import cn.e3mall.common.utils.FastDFSClient;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
 public class FastDfsTest {
-	@Test
+	
+
 	public void testUpload() throws Exception {
 		// 创建一个配置文件。文件名任意。内容就是tracker服务器的地址。
 		// 使用全局对象加载配置文件。
-		ClientGlobal.init("I:\\Django\\宜立方\\e3商城_day01\\黑马32期\\day01\\source\\e3-manager-web\\conf\\client.conf");
+		ClientGlobal.init("I:\\Django\\宜立方\\e3商城_day01\\黑马32期\\day01\\source\\e3-manager-web\\src\\main\\resources\\conf\\client.conf");
 		// 创建一个TrackerClient对象
 		TrackerClient trackerClient = new TrackerClient();
 
@@ -23,10 +27,16 @@ public class FastDfsTest {
 		// 创建一个StorageClient，参数需要TrackerServer和StrorageServer
 		StorageClient storageClient = new StorageClient(trackerServer, storageServer);
 		// 使用StorageClient上传文件。
-		String[] strings = storageClient.upload_file("C:\\Users\\24219\\Desktop\\9d52c073gy1fpieu81jr3j20p00vptod.jpg", "jpg", null);
+		String[] strings = storageClient.upload_file("C://Users//24219//Desktop//9d52c073gy1fpieu81jr3j20p00vptod.jpg", "jpg", null);
 		for (String string : strings) {
 			System.out.println(string);
 		}
+	}
+	@Test
+	public void testFastDfsClient() throws Exception{
+		FastDFSClient fastDFSClient = new FastDFSClient("I:\\Django\\宜立方\\e3商城_day01\\黑马32期\\day01\\source\\e3-manager-web\\src\\main\\resources\\conf\\client.conf");
+		String string = fastDFSClient.uploadFile("C:\\Users\\24219\\Desktop\\3db714cf36dc2f1ed8846760c14a8e59.jpg");
+		System.out.println(string);
 	}
 
 }
