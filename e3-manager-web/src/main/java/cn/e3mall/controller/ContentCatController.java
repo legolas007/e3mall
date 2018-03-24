7a6514f6509fee22bf25a681456f4754bb898f8e
legolas007 <oukuanou@163.com>
2018-03-24 17:12:08 +0800

    内容分类

---
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ContentCatController.java
@@ -0,0 +1,33 @@
+package cn.e3mall.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.content.service.ContentCategoryService;
+
+/**
+ * 内容分类管理
+ * 
+ * @author usher
+ *
+ */
+
+@Controller
+public class ContentCatController {
+
+	@Autowired
+	private ContentCategoryService contentCategoryService;
+
+	@RequestMapping("/content/category/list")
+	@ResponseBody
+	public List<EasyUITreeNode> getContentCatList(@RequestParam(name = "id", defaultValue = "0") Long parentId) {
+		List<EasyUITreeNode> list = contentCategoryService.getContentCatList(parentId);
+		return list;
+	}
+}
