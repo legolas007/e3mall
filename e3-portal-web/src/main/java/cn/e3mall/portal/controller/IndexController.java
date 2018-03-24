a8633b76d915bba2a3a5d5125903572e287ff809
legolas007 <oukuanou@163.com>
2018-03-24 23:45:13 +0800

    前台业务

--- e3-portal-web/src/main/java/cn/e3mall/portal/controller/IndexController.java
+++ e3-portal-web/src/main/java/cn/e3mall/portal/controller/IndexController.java
@@ -8,21 +8,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.e3mall.content.service.ContentService;
+import cn.e3mall.pojo.TbContent;
+
 
 /**
  * 首页展示Controller
- * <p>Title: IndexController</p>
- * <p>Description: </p>
- * <p>Company: www.itcast.cn</p > 
- * @version 1.0
  */
 @Controller
 public class IndexController {
+	@Value("${CONTENT_LUNBO_ID}")
+	private Long CONTENT_LUNBO_ID;
 	
-
+	
+	@Autowired
+	private ContentService contentService;
+	
 	@RequestMapping("/index")
 	public String showIndex(Model model) {
 		//查询内容列表
+		List<TbContent> ad1List = contentService.getContentListByCid(CONTENT_LUNBO_ID);
+		//結果傳遞到頁面
+		model.addAttribute("ad1List",ad1List);
 		
 		return "index";
 	}
