9b8394f9c81226766d40e6216a4ea3025d742891
legolas007 <oukuanou@163.com>
2018-03-24 11:24:43 +0800

    bug修复

--- e3-common/src/main/java/cn/e3mall/common/utils/IDUtils.java
+++ e3-common/src/main/java/cn/e3mall/common/utils/IDUtils.java
@@ -38,7 +38,7 @@ public class IDUtils {
 		Random random = new Random();
 		int end2 = random.nextInt(99);
 		//如果不足两位前面补0
-		String str = millis + String.format("02d", end2);
+		String str = millis + String.format("%02d", end2);
 		long id = new Long(str);
 		return id;
 	}
