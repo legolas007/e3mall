af367967c2c34520d4fe35ea58433d433019c69d
legolas007 <oukuanou@163.com>
2018-03-24 11:25:26 +0800

    商品添加controller实现

--- e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.service.ItemService;
 
@@ -31,6 +33,19 @@ public class ItemController {
 	public EasyUIDataGridResult getItemList(Integer page,Integer rows) {
 		//商品查询列表
 		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		return result;
+	}
+	
+	/**
+	 * 商品添加
+	 * @param item
+	 * @param desc
+	 * @return
+	 */
+	@RequestMapping(value="/item/save",method=RequestMethod.POST)
+	@ResponseBody
+	public E3Result addItem(TbItem item, String desc) {
+		E3Result result = itemService.addItem(item, desc);
 		return result;
 	}
 }
