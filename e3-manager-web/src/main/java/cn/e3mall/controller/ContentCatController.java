6dd9dd44d8f097396997aadc3c2cbe47c63e4d3d
legolas007 <oukuanou@163.com>
2018-03-24 19:53:49 +0800

    添加分类节点

--- e3-manager-web/src/main/java/cn/e3mall/controller/ContentCatController.java
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ContentCatController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.content.service.ContentCategoryService;
 
 /**
@@ -29,5 +31,19 @@ public class ContentCatController {
 	public List<EasyUITreeNode> getContentCatList(@RequestParam(name = "id", defaultValue = "0") Long parentId) {
 		List<EasyUITreeNode> list = contentCategoryService.getContentCatList(parentId);
 		return list;
+	}
+	
+	/**
+	 * 添加分类节点
+	 * @param parentId
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping(value="/content/category/create", method=RequestMethod.POST)
+	@ResponseBody
+	public E3Result createContentCategory(Long parentId, String name) {
+		
+		E3Result e3Result = contentCategoryService.addContentCategory(parentId, name);
+		return e3Result;
 	}
 }
