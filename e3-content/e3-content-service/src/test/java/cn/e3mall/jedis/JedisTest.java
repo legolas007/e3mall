b72a4cf1b46c6117b3b9f3e6a40a2491c67b166e
legolas007 <oukuanou@163.com>
2018-03-25 15:37:55 +0800

    JedisCluster操作集群

--- e3-content/e3-content-service/src/test/java/cn/e3mall/jedis/JedisTest.java
+++ e3-content/e3-content-service/src/test/java/cn/e3mall/jedis/JedisTest.java
@@ -1,8 +1,13 @@
 package cn.e3mall.jedis;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 
 public class JedisTest {
@@ -30,6 +35,29 @@ public class JedisTest {
 	jedis.close();
 	//关闭连接池
 	jedisPool.close();
+	}
+	
+	/**
+	 * JedisCluster连接集群
+	 * @throws Exception
+	 */
+	@Test
+	public void testJedisCluster() throws Exception{
+		//创建一个JedisCluster，参数nodes是set类型，set包含若干HostAndPort对象
+		Set<HostAndPort> nodes = new HashSet<>();
+		nodes.add(new HostAndPort("192.168.25.133", 7001));
+		nodes.add(new HostAndPort("192.168.25.133", 7002));
+		nodes.add(new HostAndPort("192.168.25.133", 7003));
+		nodes.add(new HostAndPort("192.168.25.133", 7004));
+		nodes.add(new HostAndPort("192.168.25.133", 7005));
+		nodes.add(new HostAndPort("192.168.25.133", 7006));
+		JedisCluster jedisCluster = new JedisCluster(nodes);
+		//直接使用jedisCluster操作redis
+		jedisCluster.set("test", "test JedisCluster redis-cluster");
+		String string = jedisCluster.get("test");
+		System.out.println(string);
+		//close
+		jedisCluster.close();
 	}
 	
 }
