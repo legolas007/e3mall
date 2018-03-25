9fd14fc3ea777ee7917cda4520af53eac757e877
legolas007 <oukuanou@163.com>
2018-03-25 15:26:51 +0800

    Jedis连接池

--- e3-content/e3-content-service/src/test/java/cn/e3mall/jedis/JedisTest.java
+++ e3-content/e3-content-service/src/test/java/cn/e3mall/jedis/JedisTest.java
@@ -3,6 +3,7 @@ package cn.e3mall.jedis;
 import org.junit.Test;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 public class JedisTest {
 
@@ -17,4 +18,19 @@ public class JedisTest {
 		
 		jedis.close();
 	}
+	
+	@Test
+	public void testJedisPool() throws Exception{
+	//连接池
+	JedisPool jedisPool = new JedisPool("192.168.25.133",6379);
+	//创建一个连接
+	Jedis jedis = jedisPool.getResource();
+	String string = jedis.get("test123");
+	System.out.println(string);
+	jedis.close();
+	//关闭连接池
+	jedisPool.close();
+	}
+	
 }
+	
