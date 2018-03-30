0227418a39cdfb2d8408ce6b156410c60b187a8e
legolas007 <oukuanou@163.com>
2018-03-30 23:16:34 +0800

    页面静态化

--- e3-item-web/src/test/java/cn/e3mall/freemarker/FreeMarkerTest.java
+++ e3-item-web/src/test/java/cn/e3mall/freemarker/FreeMarkerTest.java
@@ -23,12 +23,15 @@ public class FreeMarkerTest {
 		//4、模板文件的编码格式，一般就是utf-8
 		configuration.setDefaultEncoding("utf-8");
 		//5、加载一个模板文件，创建一个模板对象。
-		Template template = configuration.getTemplate("hello.ftl");
+		Template template = configuration.getTemplate("student.ftl");
 		//6、创建一个数据集。可以是pojo也可以是map。推荐使用map
 		Map data = new HashMap<>();
 		data.put("hello", "hello freemarker!");
+		//创建一个pojo对象
+		Student student = new Student(1, "小明", 18, "回龙观");
+		data.put("student", student);
 		//7、创建一个Writer对象，指定输出文件的路径及文件名。
-		Writer out = new FileWriter(new File("I:/Django/宜立方/e3商城_day01/黑马32期/day01/source/e3-item-web/src/main/webapp/WEB-INF/ftl/hello.txt"));
+		Writer out = new FileWriter(new File("I:/Django/宜立方/e3商城_day01/黑马32期/day01/source/e3-item-web/src/main/webapp/WEB-INF/ftl/hello.html"));
 		//8、生成静态页面
 		template.process(data, out);
 		//9、关闭流
