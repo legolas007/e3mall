60c1ef1703e97122c6fd7f435f41ea19ce7920c8
legolas007 <oukuanou@163.com>
2018-03-31 10:04:57 +0800

    商品添加监听器，页面静态化

--- e3-item-web/src/main/java/cn/e3mall/item/listener/HtmlGenListener.java
+++ e3-item-web/src/main/java/cn/e3mall/item/listener/HtmlGenListener.java
@@ -1,5 +1,71 @@
 package cn.e3mall.item.listener;
 
-public class HtmlGenListener {
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import cn.e3mall.item.pojo.Item;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
+import cn.e3mall.service.ItemService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+
+/**
+ * 监听商品添加，生成对应静态页面
+ * @author usher
+ *
+ */
+public class HtmlGenListener implements MessageListener{
+
+	@Autowired
+	private ItemService itemService;
+	@Autowired
+	private FreeMarkerConfigurer freeMarkerConfigurer;
+	@Value("${HTML_GEN_PATH}")
+	private String HTML_GEN_PATH;
+	
+	@Override
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+		try {
+			TextMessage textMessage = (TextMessage) message;
+			String text = textMessage.getText();
+			long itemId = new Long(text);
+			Thread.sleep(1000);
+			TbItem tbItem = itemService.getItemById(itemId);
+			Item item = new Item(tbItem);
+			TbItemDesc itemDesc = itemService.getItemDescById(itemId);
+			Map data = new HashMap<>();
+			data.put("item", item);
+			data.put("itemDesc", itemDesc);
+			//加载模板对象
+			Configuration configuration = freeMarkerConfigurer.getConfiguration();
+			Template template = configuration.getTemplate("item.ftl");
+			//创建一个输出流，指定输出的目录及文件名。
+			Writer out = new FileWriter(HTML_GEN_PATH + itemId + ".html");
+			//生成静态页面。
+			template.process(data, out);
+			//关闭流
+			out.close();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 }
