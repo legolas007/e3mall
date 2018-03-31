dc05cb95db3b042e61578408b5e7ef207c00a3f8
legolas007 <oukuanou@163.com>
2018-03-31 21:02:28 +0800

    跨域请求bug修复

--- e3-sso-web/src/main/java/cn/e3mall/sso/controller/TokenController.java
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/TokenController.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +19,7 @@ import cn.e3mall.sso.service.TokenService;
  * @author usher
  *
  */
+@Controller
 public class TokenController {
 
 	@Autowired
