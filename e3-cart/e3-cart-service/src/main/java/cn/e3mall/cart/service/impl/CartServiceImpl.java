59130a239c565ec3beba1923a75fc667d09904b2
legolas007 <oukuanou@163.com>
2018-04-04 17:57:27 +0800

    订单详情，删除购物车

--- e3-cart/e3-cart-service/src/main/java/cn/e3mall/cart/service/impl/CartServiceImpl.java
+++ e3-cart/e3-cart-service/src/main/java/cn/e3mall/cart/service/impl/CartServiceImpl.java
@@ -108,4 +108,13 @@ public class CartServiceImpl implements CartService {
 		return E3Result.ok();
 	}
 
+	@Override
+	public E3Result clearCartItem(long userId) {
+		// TODO Auto-generated method stub
+		//删除购物车信息
+		
+		jedisClient.del(REDIS_CART_PRE + ":" + userId);
+		return E3Result.ok();
+	}
+
 }
